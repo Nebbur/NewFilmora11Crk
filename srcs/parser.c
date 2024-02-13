a516cd7b434315ca7babe3a234acff637059f387
Nebbuur <sp.10.me.18@gmail.com>
2024-02-13 10:51:37 +0000

    validation ft improved

--- srcs/parser.c
+++ srcs/parser.c
@@ -26,27 +26,20 @@ void	new_input(t_shell *shell)
 
 int	validation(t_shell *shell)
 {
-	int		i;
 	t_token	*token;
 
 	token = shell->token;
-	i = 0;
 	while (token)
 	{
+		if (token->type == S_QUOTE || token->type == D_QUOTE)
+			token->type = WORD;
 		if (token->type == BACKSLASH)
-		{
 			if (token->next == NULL)
-			{
-				printf("Error: Backslash not followed by any character\n");
-				return (1);
-			}
-		}
+				new_input(shell);
 		if (token->type == PIPE || token->type == AND || token->type == OR)
 		{
 			if (token->next == NULL)
-			{
 				new_input(shell);
-			}
 			if (token->next->type == PIPE || token->next->type == AND || token->next->type == OR)
 			{
 				printf("Error: Operator not followed by any command\n");
@@ -55,22 +48,9 @@ int	validation(t_shell *shell)
 		}
 		if (token->type == REDIR_OUT || token->type == REDIR_IN || token->type == REDIR_APPEND || token->type == REDIR_HEREDOC)
 		{
-			if (token->next == NULL)
+			if (token->next == NULL || token->next->type == PIPE || token->next->type == AND || token->next->type == OR)
 			{
 				printf("Error: Redirection not followed by any file\n");
-				return (1);
-			}
-			if (token->next->type == PIPE || token->next->type == AND || token->next->type == OR)
-			{
-				printf("Error: Redirection not followed by any file\n");
-				return (1);
-			}
-		}
-		if (token->type == SEMICOLON)
-		{
-			if (token->next == NULL)
-			{
-				printf("Error: Semicolon not followed by any command\n");
 				return (1);
 			}
 		}
@@ -89,8 +69,6 @@ int	parsing(t_shell *shell)
 	while (temp)
 	{
 		printf("Token: %s\n", temp->value);
-		/* if (temp->type == S_QUOTE || temp->type == D_QUOTE)
-			temp->type = WORD; */
 		if (temp->type == WORD)
 		{
 			shell->cmds[i] = ft_strdup(temp->value);
